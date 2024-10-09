82037763b8d71732d6ad2cfecc4a80783b1b9ef5
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-09 22:37:41 +0300

    Add migration scripts

---
+++ src/main/resources/db/migration/V1__init-schema.sql
@@ -0,0 +1,13 @@
+create table if not exists `sales`
+(
+    `sale_id`        bigint primary key auto_increment,
+    `product_id`     int            not null,
+    `customer_id`    int            not null,
+    `sale_date`      datetime       not null,
+    `sale_amount`    decimal(10, 2) not null,
+    `store_location` varchar(100)   not null,
+    `country`        varchar(100)   not null,
+    `processed`      bit            null
+
+);
+
