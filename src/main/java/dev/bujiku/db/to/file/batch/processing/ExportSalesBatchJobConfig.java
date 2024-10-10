2ff17169a7251f05e0b09cab126c09342e4a522d
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-10 14:27:25 +0300

    Add item writer

--- src/main/java/dev/bujiku/db/to/file/batch/processing/ExportSalesBatchJobConfig.java
+++ src/main/java/dev/bujiku/db/to/file/batch/processing/ExportSalesBatchJobConfig.java
@@ -2,12 +2,20 @@ package dev.bujiku.db.to.file.batch.processing;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileHeaderCallback;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.Writer;
 
 /**
  * @author Newton Bujiku
@@ -18,6 +26,8 @@ import javax.sql.DataSource;
 public class ExportSalesBatchJobConfig {
     private final DataSource dataSource;
 
+    @Bean
+    @StepScope
     public JdbcCursorItemReader<Sale> salesItemReader(@Value("#{jobParameters}") JobParameters parameters) {
         var processed = parameters.getParameters().get("processed");
         var sql = """
@@ -32,6 +42,24 @@ public class ExportSalesBatchJobConfig {
                 .fetchSize(100)
                 .rowMapper(new SalesRowMapper())
                 .queryArguments(processed)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<Sale> salesFlatFileItemWriter(@Value("#{jobParameters['filename']}") String filename) {
+
+        return new FlatFileItemWriterBuilder<Sale>()
+                .name("salesWriter")
+                .resource(new FileSystemResource(filename))
+                .delimited()
+                .delimiter(",")
+                .sourceType(Sale.class)
+                .names("product_id", "customer_id", "sale_date", "sale_amount", "store_location", "country")
+                .shouldDeleteIfEmpty(true)
+                .headerCallback(writer -> {
+                    writer.append("Header");
+                })
                 .build();
     }
 
