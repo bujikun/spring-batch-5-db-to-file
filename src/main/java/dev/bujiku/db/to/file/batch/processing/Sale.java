014f37ebde0d724628520610752459a16fb706dd
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-10 13:15:38 +0300

    Change sale domain

--- src/main/java/dev/bujiku/db/to/file/batch/processing/Sale.java
+++ src/main/java/dev/bujiku/db/to/file/batch/processing/Sale.java
@@ -1,11 +1,6 @@
 package dev.bujiku.db.to.file.batch.processing;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -14,20 +9,13 @@ import java.time.Instant;
  * @author Newton Bujiku
  * @since 2024
  */
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
 @Getter
-@Table(name = "sales")
-public class Sale {
-    @Id
-    private Long saleId;
-    private Integer productId;
-    private Integer customerId;
-    private Instant saleDate;
-    private BigDecimal saleAmount;
-    private String storeLocation;
-    private String country;
-    private Boolean processed;
-
+public record Sale(Long saleId,
+                   Integer productId,
+                   Integer customerId,
+                   Instant saleDate,
+                   BigDecimal saleAmount,
+                   String storeLocation,
+                   String country,
+                   Boolean processed) {
 }
