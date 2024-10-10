b3999bf51d74519deef727daedd630b515b07dbd
Newton Bujiku <bujikun@vodacom.co.tz>
2024-10-10 16:29:59 +0300

    Add write listener to update sale after processing

--- src/main/java/dev/bujiku/db/to/file/batch/processing/ExportSalesBatchJobConfig.java
+++ src/main/java/dev/bujiku/db/to/file/batch/processing/ExportSalesBatchJobConfig.java
@@ -2,17 +2,14 @@ package dev.bujiku.db.to.file.batch.processing;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.skip.AlwaysSkipItemSkipPolicy;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
-import org.springframework.batch.item.file.FlatFileHeaderCallback;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,8 +21,8 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.Writer;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 /**
  * @author Newton Bujiku
@@ -37,15 +34,16 @@ public class ExportSalesBatchJobConfig {
     private final DataSource dataSource;
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
+    private final SalesUpdateListener salesUpdateListener;
 
     @Bean
     @StepScope
-    public JdbcCursorItemReader<Sale> salesItemReader(@Value("#{jobParameters}") JobParameters parameters) {
-        var processed = parameters.getParameters().get("processed");
+    public JdbcCursorItemReader<Sale> salesItemReader(@Value("#{jobParameters}") Map<String, Object> parameters) {
+        var processed = parameters.get("processed");
         var sql = """
-                SELECT `product_id`, `customer_id`, `sale_date`, `sale_amount`, `store_location`, `country`
-                FROM `sales`
-                WHERE `processed`=?1
+                SELECT sale_id, product_id, customer_id, sale_date, sale_amount, store_location, country,processed
+                FROM sales
+                WHERE processed=?
                 """;
         return new JdbcCursorItemReaderBuilder<Sale>()
                 .name("salesReader")
@@ -53,6 +51,7 @@ public class ExportSalesBatchJobConfig {
                 .sql(sql)
                 .fetchSize(100)
                 .rowMapper(new SalesRowMapper())
+                .verifyCursorPosition(false)
                 .queryArguments(processed)
                 .build();
     }
@@ -60,31 +59,33 @@ public class ExportSalesBatchJobConfig {
     @Bean
     @StepScope
     public FlatFileItemWriter<Sale> salesFlatFileItemWriter(@Value("#{jobParameters['filename']}") String filename) {
-
         return new FlatFileItemWriterBuilder<Sale>()
                 .name("salesWriter")
                 .resource(new FileSystemResource(filename))
                 .delimited()
                 .delimiter(",")
                 .sourceType(Sale.class)
-                .names("product_id", "customer_id", "sale_date", "sale_amount", "store_location", "country")
+                .names("product_id", "customer_id", "sale_date", "sale_amount", "store_location", "country","processed")
                 .shouldDeleteIfEmpty(true)
                 .headerCallback(writer -> {
-                    writer.append("Header");
+                    writer.append("sale_id, product_id, customer_id, sale_date, sale_amount, store_location, country,processed");
                 })
                 .build();
     }
 
     @Bean
-    public Step readFromDBAndWriteToFileStep(JdbcCursorItemReader<Sale> salesItemReader, FlatFileItemWriter<Sale> salesFlatFileItemWriter) {
+    public Step readFromDBAndWriteToFileStep(JdbcCursorItemReader<Sale> salesItemReader,
+                                             FlatFileItemWriter<Sale> salesFlatFileItemWriter) {
         return new StepBuilder("readFromDBAndWriteToFileStep", jobRepository)
                 .<Sale, Sale>chunk(200, platformTransactionManager)
                 .reader(salesItemReader)
                 .processor(new SalesItemProcessor())
                 .writer(salesFlatFileItemWriter)
                 .faultTolerant()
-                .skipPolicy(new AlwaysSkipItemSkipPolicy())
-                .taskExecutor(taskExecutor())
+                .listener(new SalesItemReadListener())
+                .listener(salesUpdateListener)
+                //.skipPolicy(new AlwaysSkipItemSkipPolicy())
+                //.taskExecutor(taskExecutor())
                 .build();
     }
 
@@ -97,7 +98,8 @@ public class ExportSalesBatchJobConfig {
 
     @Bean
     public Job readFromDBAndWriteToFileJob(Step readFromDBAndWriteToFileStep) {
-        return new JobBuilder("readFromDBAndWriteToFileJob", jobRepository)
+        var now = LocalDateTime.now().toString();
+        return new JobBuilder("readFromDBAndWriteToFileJob-" + now, jobRepository)
                 .start(readFromDBAndWriteToFileStep)
                 .incrementer(new RunIdIncrementer())
                 .build();
---
+++ src/main/java/dev/bujiku/db/to/file/batch/processing/SalesUpdateListener.java
@@ -0,0 +1,31 @@
+package dev.bujiku.db.to.file.batch.processing;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ItemWriteListener;
+import org.springframework.batch.item.Chunk;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Newton Bujiku
+ * @since 2024
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SalesUpdateListener implements ItemWriteListener<Sale> {
+    private final JdbcClient jdbcClient;
+
+    @Override
+    public void afterWrite(Chunk<? extends Sale> items) {
+        items.forEach(this::updateSaleProcessedStatus);
+    }
+
+    private void updateSaleProcessedStatus(Sale sale) {
+        jdbcClient.sql("UPDATE sales SET processed=:processed WHERE sale_id=:saleId")
+                .param("processed", 1)
+                .param("saleId", sale.saleId())
+                .update();
+    }
+}
